9e63c66d9bdbcc07b70e4457bf846a6f5b20f551
Vinicius Menezes <vini.ads13@outlook.com>
2026-05-05 21:40:48 -0300

    :rocket: adicionando a versão 3.0 da aplicação com pipeline CICD

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Aplicacao exemplo - 2.0")
+	fmt.Fprintf(w, "Aplicacao exemplo - 3.0 - Deus é bom - Virei oficialmente um DEVOPS - EU amo a tefanildinha")
 }
 
 func main() {
